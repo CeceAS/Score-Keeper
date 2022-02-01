9b96055336e947b104d77c1230ea011d8fe8d337
Cece <cecesanclemente@gmail.com>
2022-02-01 04:13:28 -0600

    Switch code to OOP

---
+++ script-alternate.js
@@ -0,0 +1,300 @@
+'use strict';
+
+// const clockSpot = document.querySelector('.clock');
+
+// const displayClock = function () {
+//   const time = new Date();
+
+//   const options = {
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     second: 'numeric',
+//   };
+
+//   const timeFormat = Intl.DateTimeFormat('en-US', options).format(time);
+//   clockSpot.textContent = timeFormat;
+// };
+
+// setInterval(displayClock, 1000);
+
+// displayClock();
+
+// note: FLOW CHART
+
+// player adds name -> name appears in players.
+// game starts. First player scores, socre is added to that player -> total score is updated
+
+// UNIT: DOM Elements:
+
+const containerPlayers = document.querySelector('.players-container');
+const containerAddPlayers = document.querySelector('.add__players-container');
+const containerAddResetPlayers = document.querySelector(
+  '.add_resetplayers-container'
+);
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn-close-modal');
+const btnOpenModal = document.querySelector('.btn-show-modal');
+
+const nav = document.querySelector('nav');
+
+const labelPlayerName = document.querySelector('player__name');
+const labelTotalScore = document.querySelectorAll('.player__score');
+
+const newScoreInput = document.querySelector('.score__input');
+const newScoreInputs = document.querySelectorAll('.score__input');
+
+const inputPlayerName = document.querySelector('.player__name-input');
+
+// const btnAddScore = document.querySelector('.form__btn--add-score');
+
+const btnsAddScore = document.querySelectorAll('.form__btn--add-score');
+const btnAddPlayer = document.querySelector('.form__btn--add-player');
+const btnAddMorePlayers = document.querySelector('.add-more-players');
+// const btnResetPlayers = document.querySelector('.reset-players');
+const btnLetsPlay = document.querySelector('.lets__play-btn');
+
+// const playerNamesArray = document.querySelectorAll('.player__name');
+
+// UNIT: Show & Hide Modal
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+btnOpenModal.addEventListener('click', openModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+// UNIT:  Default players:
+
+const playerstest = [
+  {
+    name: 'Cynthia',
+    scores: [10, 25, 5, 6],
+    totalScore: 0,
+  },
+  {
+    name: 'Brayden',
+    scores: [10, 5, 5, 6],
+    totalScore: 0,
+  },
+];
+
+let allPlayers = [];
+
+// UNIT: Data:
+
+//  array of add score btns:
+const btnsAddScoreArray = Array.from(btnsAddScore);
+
+// array of inputs:
+const newScoreInputsArray = Array.from(newScoreInputs);
+
+// UNIT: Refresh if user confirms:
+
+window.addEventListener('beforeunload', event => {
+  event.returnValue = `Are you sure you want to leave?`;
+});
+
+// UNIT: Functions
+
+// function to calculate total score
+const calcAndDisplayTotalScore = function () {
+  allPlayers.forEach(function (player, i) {
+    // Calculate each player total score:
+    player.scores.reduce((acc, score, i) => (player.totalScore = acc + score));
+
+    // Display Score:
+    let scoreDisplays = document.querySelectorAll('.player__score');
+    let scoreDisplaysArray = Array.from(scoreDisplays);
+
+    let labelScoresArray = document.querySelectorAll('.scores-array');
+    let labelTotalScoreArray = labelScoresArray;
+
+    scoreDisplaysArray[i].textContent = allPlayers[i].totalScore;
+
+    // Add new score to displayed array:
+    let displayedScoreArr = player.scores.slice(1).join(', ');
+    labelTotalScoreArray[i].textContent = displayedScoreArr;
+  });
+};
+
+// Add event listener to add new score buttons:
+const addNewScore = function () {
+  // arrays:
+  let scoreInputs = document.querySelectorAll('.score__input');
+  let scoreInputsArray = Array.from(scoreInputs);
+
+  let addScoresBtns = document.querySelectorAll('.form__btn--add-score');
+  let addScoresBtnsArray = Array.from(addScoresBtns);
+
+  addScoresBtnsArray.forEach(function (btn, i, btnArray) {
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      const newScore = +scoreInputsArray[i].value;
+
+      if (newScore !== 0) {
+        allPlayers[i].scores.push(newScore);
+        console.log(allPlayers);
+      }
+
+      // Display total score:
+      calcAndDisplayTotalScore();
+
+      // Emptying input once score is added:
+      scoreInputsArray[i].value = '';
+
+      // Display current winner:
+      document.querySelector('.winner-container-title').classList.add('hidden');
+
+      const scoresMap = new Map();
+
+      allPlayers.forEach(player => {
+        scoresMap.set(player.name, player.totalScore);
+      });
+
+      const sortedScoresMap = new Map(
+        [...scoresMap.entries()].sort((a, b) => b[1] - a[1])
+      );
+
+      const mapIterator = sortedScoresMap.entries();
+
+      const playersOrdered = [];
+      const scoresOrdered = [];
+      const winnerContainer = document.querySelector('.winner');
+      let winners = [];
+      const winnerTie = document.querySelector('.winner-tie');
+      let winnersTextStart;
+      let winnersTextEnd;
+
+      for (const [playerName, score] of mapIterator) {
+        playersOrdered.push(playerName);
+        scoresOrdered.push(score);
+      }
+
+      console.log('players:', playersOrdered, 'scores', scoresOrdered);
+
+      // if there's ties or no ties:
+      if (scoresOrdered[0] !== scoresOrdered[1]) {
+        winnerTie.classList.add('hidden');
+        winnerContainer.innerHTML = `
+        <h2><span class="winner-title">${playersOrdered[0]}</span> is winning with a score of <span class="winner-title">${scoresOrdered[0]}</span> 🥳</h2>`;
+      }
+
+      if (scoresOrdered[0] === scoresOrdered[1]) {
+        winnerContainer.textContent = '';
+        winnerTie.classList.remove('hidden');
+
+        scoresOrdered.forEach((score, i, arr) => {
+          if (arr[1] === score) {
+            winners.push(playersOrdered[i]);
+          }
+        });
+
+        if (winners.length === 2) {
+          winnersTextStart = winners[0];
+        }
+        if (winners.length > 2) {
+          winnersTextStart = winners.slice(0, -1).join(', ');
+        }
+
+        console.log('winners are:', winners);
+        winnersTextEnd = winners.slice(-1);
+
+        winnerContainer.innerHTML = `<h2><span class="winner-title">${winnersTextStart}</span> and <span class="winner-title">${winnersTextEnd}</span> are tied with a score of <span class="winner-title">${scoresOrdered[0]}</span> 🥳</h2>`;
+      }
+    });
+  });
+};
+
+const addPlayers = function () {
+  btnAddPlayer.addEventListener('click', function () {
+    if (document.querySelector('.player__name-input').value.length > 0) {
+      const playerName = document.querySelector('.player__name-input').value;
+      let playerObject = {
+        name: playerName,
+        scores: [0],
+        totalScore: 0,
+      };
+
+      allPlayers.push(playerObject);
+      console.log(playerObject);
+      console.log(allPlayers);
+
+      let html;
+      allPlayers.forEach(function (curPlayer, i) {
+        html = `
+      <div class="player__row">
+      <div class="player__name-container">
+        <p class="player__number-label">Player ${i + 1}</p>
+        <div class="player__name">${curPlayer.name}</div>
+        <div><p><strong>Scores:  </strong><span class="scores-array"></span></p></div>
+      </div>
+      <div class="flex-row">
+      <div class="score__input-container">
+        <input id="" type="number" class="score__input form__input--amount"/>
+        <button id="" class="btn form__btn form__btn--add-score">+</button>
+      </div>
+      <div class="total__score-container">
+        <p class="score-label">Score</p>
+        <div id="${curPlayer.name}-display-score" class="player__score ${
+          curPlayer.name
+        }">
+        0</div>
+      </div>
+      </div>
+    </div>
+    
+      `;
+      });
+
+      // remove hidden class from container of players:
+      containerPlayers.classList.remove('hidden');
+
+      containerPlayers.insertAdjacentHTML('beforeend', html);
+
+      addNewScore();
+
+      // console.log('Children:', containerPlayers.children);
+
+      // Empty new player input on click:
+      document.querySelector('.player__name-input').value = '';
+    }
+  });
+};
+
+addPlayers();
+
+// Hide add player container when "Let's Play is clicked:"
+
+btnLetsPlay.addEventListener('click', function (e) {
+  if (!containerPlayers.classList.contains('hidden')) {
+    containerAddPlayers.classList.add('hidden');
+    containerAddResetPlayers.classList.remove('hidden');
+  }
+});
+
+// Show Add player container when "Add More Players" is clicked:
+
+btnAddMorePlayers.addEventListener('click', function (e) {
+  containerAddPlayers.classList.remove('hidden');
+  btnLetsPlay.textContent = 'Keep Playing';
+  containerAddResetPlayers.classList.add('hidden');
+  nav.querySelector('p').classList.remove('hidden');
+});
--- script.js
+++ script.js
@@ -1,300 +1,288 @@
 'use strict';
 
-// const clockSpot = document.querySelector('.clock');
-
-// const displayClock = function () {
-//   const time = new Date();
-
-//   const options = {
-//     hour: 'numeric',
-//     minute: 'numeric',
-//     second: 'numeric',
-//   };
-
-//   const timeFormat = Intl.DateTimeFormat('en-US', options).format(time);
-//   clockSpot.textContent = timeFormat;
-// };
-
-// setInterval(displayClock, 1000);
-
-// displayClock();
-
-// note: FLOW CHART
-
-// player adds name -> name appears in players.
-// game starts. First player scores, socre is added to that player -> total score is updated
-
-// UNIT: DOM Elements:
+const addPlayerBtn = document.querySelector('.form__btn--add-player');
+const letsPlayBtn = document.querySelector('.lets__play-btn');
+const colorPickerBtn = document.querySelector('#color-picker');
+const addNewScoreBtns = document.querySelectorAll('.form__btn--add-score');
+const addMorePlayersBtn = document.querySelector('.add-more-players');
 
 const containerPlayers = document.querySelector('.players-container');
 const containerAddPlayers = document.querySelector('.add__players-container');
-const containerAddResetPlayers = document.querySelector(
-  '.add_resetplayers-container'
+const containerScoreInput = document.querySelector('.score__input-container');
+const containerAddMorePlayers = document.querySelector(
+  '.add_more_players-container'
 );
+
+const playerNameInput = document.querySelector('.player__name-input');
+const newScoreInput = document.querySelector('.score__input');
+
+const playerScoreLabel = document.querySelector('.player__score');
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn-close-modal');
 const btnOpenModal = document.querySelector('.btn-show-modal');
 
-const nav = document.querySelector('nav');
+class Player {
+  id = (Date.now() + '').slice(-10);
+  scores = [];
+  totalScore = 0;
 
-const labelPlayerName = document.querySelector('player__name');
-const labelTotalScore = document.querySelectorAll('.player__score');
-
-const newScoreInput = document.querySelector('.score__input');
-const newScoreInputs = document.querySelectorAll('.score__input');
-
-const inputPlayerName = document.querySelector('.player__name-input');
-
-// const btnAddScore = document.querySelector('.form__btn--add-score');
-
-const btnsAddScore = document.querySelectorAll('.form__btn--add-score');
-const btnAddPlayer = document.querySelector('.form__btn--add-player');
-const btnAddMorePlayers = document.querySelector('.add-more-players');
-// const btnResetPlayers = document.querySelector('.reset-players');
-const btnLetsPlay = document.querySelector('.lets__play-btn');
-
-// const playerNamesArray = document.querySelectorAll('.player__name');
-
-// UNIT: Show & Hide Modal
-
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-btnCloseModal.addEventListener('click', closeModal);
-
-overlay.addEventListener('click', closeModal);
-
-btnOpenModal.addEventListener('click', openModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+  constructor(playerName, color) {
+    this.playerName = playerName;
+    this.color = color;
   }
-});
+}
 
-// UNIT:  Default players:
+// test data:
 
-const playerstest = [
-  {
-    name: 'Cynthia',
-    scores: [10, 25, 5, 6],
-    totalScore: 0,
-  },
-  {
-    name: 'Brayden',
-    scores: [10, 5, 5, 6],
-    totalScore: 0,
-  },
-];
+const cece = new Player('Cece');
+const bray = new Player('Bray');
 
-let allPlayers = [];
+////////////////////// Application Architecture //////////////////////
 
-// UNIT: Data:
+class App {
+  #players = [];
 
-//  array of add score btns:
-const btnsAddScoreArray = Array.from(btnsAddScore);
+  constructor() {
+    // add event handlers
+    addPlayerBtn.addEventListener('click', this._newPlayer.bind(this));
 
-// array of inputs:
-const newScoreInputsArray = Array.from(newScoreInputs);
+    letsPlayBtn.addEventListener('click', this._hideForm.bind(this));
 
-// UNIT: Refresh if user confirms:
+    addMorePlayersBtn.addEventListener(
+      'click',
+      this._addMorePlayers.bind(this)
+    );
 
-window.addEventListener('beforeunload', event => {
-  event.returnValue = `Are you sure you want to leave?`;
-});
+    // hiding and showing Instructions modal:
+    btnCloseModal.addEventListener('click', this._hideModal.bind(this));
+    overlay.addEventListener('click', this._hideModal.bind(this));
+    btnOpenModal.addEventListener('click', this._showModal.bind(this));
 
-// UNIT: Functions
+    document.addEventListener('keydown', this._hideModal.bind(this));
+  }
 
-// function to calculate total score
-const calcAndDisplayTotalScore = function () {
-  allPlayers.forEach(function (player, i) {
-    // Calculate each player total score:
-    player.scores.reduce((acc, score, i) => (player.totalScore = acc + score));
+  _newPlayer() {
+    //  form data:
+    const playerName = playerNameInput.value.toUpperCase();
+    const playerColor = colorPickerBtn.value;
 
-    // Display Score:
-    let scoreDisplays = document.querySelectorAll('.player__score');
-    let scoreDisplaysArray = Array.from(scoreDisplays);
+    let player;
+    let canAddPlayer = true;
 
-    let labelScoresArray = document.querySelectorAll('.scores-array');
-    let labelTotalScoreArray = labelScoresArray;
+    // checking if name is valid:
+    if (playerName.length === 0) return;
 
-    scoreDisplaysArray[i].textContent = allPlayers[i].totalScore;
-
-    // Add new score to displayed array:
-    let displayedScoreArr = player.scores.slice(1).join(', ');
-    labelTotalScoreArray[i].textContent = displayedScoreArr;
-  });
-};
-
-// Add event listener to add new score buttons:
-const addNewScore = function () {
-  // arrays:
-  let scoreInputs = document.querySelectorAll('.score__input');
-  let scoreInputsArray = Array.from(scoreInputs);
-
-  let addScoresBtns = document.querySelectorAll('.form__btn--add-score');
-  let addScoresBtnsArray = Array.from(addScoresBtns);
-
-  addScoresBtnsArray.forEach(function (btn, i, btnArray) {
-    btn.addEventListener('click', function (e) {
-      e.preventDefault();
-      const newScore = +scoreInputsArray[i].value;
-
-      if (newScore !== 0) {
-        allPlayers[i].scores.push(newScore);
-        console.log(allPlayers);
-      }
-
-      // Display total score:
-      calcAndDisplayTotalScore();
-
-      // Emptying input once score is added:
-      scoreInputsArray[i].value = '';
-
-      // Display current winner:
-      document.querySelector('.winner-container-title').classList.add('hidden');
-
-      const scoresMap = new Map();
-
-      allPlayers.forEach(player => {
-        scoresMap.set(player.name, player.totalScore);
-      });
-
-      const sortedScoresMap = new Map(
-        [...scoresMap.entries()].sort((a, b) => b[1] - a[1])
-      );
-
-      const mapIterator = sortedScoresMap.entries();
-
-      const playersOrdered = [];
-      const scoresOrdered = [];
-      const winnerContainer = document.querySelector('.winner');
-      let winners = [];
-      const winnerTie = document.querySelector('.winner-tie');
-      let winnersTextStart;
-      let winnersTextEnd;
-
-      for (const [playerName, score] of mapIterator) {
-        playersOrdered.push(playerName);
-        scoresOrdered.push(score);
-      }
-
-      console.log('players:', playersOrdered, 'scores', scoresOrdered);
-
-      // if there's ties or no ties:
-      if (scoresOrdered[0] !== scoresOrdered[1]) {
-        winnerTie.classList.add('hidden');
-        winnerContainer.innerHTML = `
-        <h2><span class="winner-title">${playersOrdered[0]}</span> is winning with a score of <span class="winner-title">${scoresOrdered[0]}</span> 🥳</h2>`;
-      }
-
-      if (scoresOrdered[0] === scoresOrdered[1]) {
-        winnerContainer.textContent = '';
-        winnerTie.classList.remove('hidden');
-
-        scoresOrdered.forEach((score, i, arr) => {
-          if (arr[1] === score) {
-            winners.push(playersOrdered[i]);
-          }
-        });
-
-        if (winners.length === 2) {
-          winnersTextStart = winners[0];
-        }
-        if (winners.length > 2) {
-          winnersTextStart = winners.slice(0, -1).join(', ');
-        }
-
-        console.log('winners are:', winners);
-        winnersTextEnd = winners.slice(-1);
-
-        winnerContainer.innerHTML = `<h2><span class="winner-title">${winnersTextStart}</span> and <span class="winner-title">${winnersTextEnd}</span> are tied with a score of <span class="winner-title">${scoresOrdered[0]}</span> 🥳</h2>`;
+    this.#players.forEach(player => {
+      if (player.playerName === playerName) {
+        playerNameInput.value = '';
+        alert('Players cannot have the same name!');
+        return (canAddPlayer = false);
       }
     });
-  });
-};
 
-const addPlayers = function () {
-  btnAddPlayer.addEventListener('click', function () {
-    if (document.querySelector('.player__name-input').value.length > 0) {
-      const playerName = document.querySelector('.player__name-input').value;
-      let playerObject = {
-        name: playerName,
-        scores: [0],
-        totalScore: 0,
-      };
+    if (canAddPlayer) {
+      player = new Player(playerName, playerColor);
+      this.#players.push(player);
+      console.log('Players:', this.#players);
 
-      allPlayers.push(playerObject);
-      console.log(playerObject);
-      console.log(allPlayers);
+      // display players container if it's hidden:
+      containerPlayers.classList.remove('hidden');
 
-      let html;
-      allPlayers.forEach(function (curPlayer, i) {
-        html = `
-      <div class="player__row">
+      // render player on page:
+      this._renderNewPlayer(player);
+    }
+
+    // clear input:
+    playerNameInput.value = '';
+  }
+
+  _renderNewPlayer(player) {
+    let html = `
+      <div class="player__row" id="${player.id}" data-id="${player.id}">
       <div class="player__name-container">
-        <p class="player__number-label">Player ${i + 1}</p>
-        <div class="player__name">${curPlayer.name}</div>
-        <div><p><strong>Scores:  </strong><span class="scores-array"></span></p></div>
+        <p class="player__number-label"></p>
+        <div class="player__name" style="background-color: ${player.color}">${player.playerName}</div>
+        <div><p><strong>Scores: </strong><span class="scores-array-${player.id}"></span></p></div>
       </div>
       <div class="flex-row">
-      <div class="score__input-container">
-        <input id="" type="number" class="score__input form__input--amount"/>
-        <button id="" class="btn form__btn form__btn--add-score">+</button>
+      <div class="score__input-container ${player.id}-score__input-container">
+        <input id="" type="number" class="add-score-${player.id}-input score__input form__input--amount" style="border:2px solid ${player.color};color: ${player.color}"/>
+        <button id="add-score-${player.id}" data-id="${player.id}" class="btn form__btn form__btn--add-score" style="border:2px solid ${player.color};background-image:none;background-color: ${player.color}">+</button>
       </div>
       <div class="total__score-container">
         <p class="score-label">Score</p>
-        <div id="${curPlayer.name}-display-score" class="player__score ${
-          curPlayer.name
-        }">
-        0</div>
+        <div id="display-score-${player.id}" class="player__score">
+       0</div>
       </div>
       </div>
-    </div>
-    
-      `;
+      </div>
+    `;
+
+    containerPlayers.insertAdjacentHTML('beforeend', html);
+
+    // add event listener to each player container:
+    document
+      .getElementById(`add-score-${player.id}`)
+      .addEventListener('click', this._addNewScore.bind(this));
+  }
+
+  _addNewScore(e) {
+    const targetID = e.target.id;
+    const targetPlayerID = e.target.dataset.id;
+    const targetPlayerIDNumber = +e.target.dataset.id;
+    const newScore = +document.querySelector(`.${targetID}-input`).value;
+
+    if (newScore === 0) return;
+
+    this.#players.forEach(function (player) {
+      if (player.id === targetPlayerID) {
+        player.scores.push(newScore);
+        console.log(player);
+      }
+    });
+
+    // add new score to scores array:
+    this._displayScoresArray();
+
+    // calculate total score:
+    this._calcAndDisplayTotalScore();
+
+    // clear score input:
+    document.querySelector(`.${targetID}-input`).value = '';
+
+    // show winner:
+    this._showCurrentWinner();
+  }
+
+  _displayScoresArray() {
+    this.#players.forEach(function (player) {
+      document.querySelector(`.scores-array-${player.id}`).textContent =
+        player.scores.join(', ');
+    });
+  }
+
+  _calcAndDisplayTotalScore() {
+    this.#players.forEach(function (player, i) {
+      player.scores.reduce(
+        (acc, score) => (player.totalScore = acc + score),
+        0
+      );
+
+      const totalScoreDisplay = document.getElementById(
+        `display-score-${player.id}`
+      );
+
+      totalScoreDisplay.textContent = player.totalScore;
+    });
+  }
+
+  _hideForm() {
+    if (!containerPlayers.classList.contains('hidden')) {
+      containerAddPlayers.classList.add('hidden');
+      containerAddMorePlayers.classList.remove('hidden');
+    }
+  }
+
+  _addMorePlayers() {
+    containerAddPlayers.classList.remove('hidden');
+    containerAddMorePlayers.classList.add('hidden');
+    // playerNameInput.focus();
+  }
+
+  _showCurrentWinner() {
+    const winnerContainer = document.querySelector('.winner');
+    const winnerTie = document.querySelector('.winner-tie');
+
+    // hide container title:
+    document.querySelector('.winner-container-title').classList.add('hidden');
+
+    // use Map to store key value pairs:
+    const scoresMap = new Map();
+
+    this.#players.forEach((player, i) => {
+      scoresMap.set(player.playerName, player.totalScore);
+    });
+
+    // sorting the scores and winners in order:
+    const sortedScoresMap = new Map(
+      [...scoresMap.entries()].sort((a, b) => b[1] - a[1])
+    );
+
+    const mapIterator = sortedScoresMap.entries();
+
+    const playersOrdered = [];
+    const scoresOrdered = [];
+    let winners = [];
+    let winnersTextStart;
+    let winnersTextEnd;
+
+    winnerContainer.textContent = '';
+
+    for (const [playerName, score] of mapIterator) {
+      playersOrdered.push(playerName);
+      scoresOrdered.push(score);
+    }
+
+    console.log('players:', playersOrdered, 'scores', scoresOrdered);
+
+    // if there's no tie;
+    if (scoresOrdered[0] !== scoresOrdered[1]) {
+      winnerTie.classList.add('hidden');
+
+      winnerContainer.innerHTML = `
+      <h2><span class="winner-title">${playersOrdered[0]}</span> is winning with a score of <span class="winner-title">${scoresOrdered[0]}</span> 🥳</h2>`;
+    }
+
+    // if there's a tie
+    if (scoresOrdered[0] === scoresOrdered[1]) {
+      winnerContainer.textContent = '';
+      winnerTie.classList.remove('hidden');
+
+      scoresOrdered.forEach((score, i, arr) => {
+        if (score === arr[1]) {
+          winners.push(playersOrdered[i]);
+        }
       });
 
-      // remove hidden class from container of players:
-      containerPlayers.classList.remove('hidden');
+      if (winners.length === 2) {
+        winnersTextStart = winners[0];
+      }
 
-      containerPlayers.insertAdjacentHTML('beforeend', html);
+      if (winners.length > 2) {
+        winnersTextStart = winners.slice(0, -1).join(', ');
+      }
 
-      addNewScore();
+      winnersTextEnd = winners.slice(-1);
 
-      // console.log('Children:', containerPlayers.children);
-
-      // Empty new player input on click:
-      document.querySelector('.player__name-input').value = '';
+      winnerContainer.innerHTML = `<h2><span class="winner-title">${winnersTextStart}</span> and <span class="winner-title">${winnersTextEnd}</span> are tied with a score of <span class="winner-title">${scoresOrdered[0]}</span> 🥳</h2>`;
     }
-  });
-};
-
-addPlayers();
-
-// Hide add player container when "Let's Play is clicked:"
-
-btnLetsPlay.addEventListener('click', function (e) {
-  if (!containerPlayers.classList.contains('hidden')) {
-    containerAddPlayers.classList.add('hidden');
-    containerAddResetPlayers.classList.remove('hidden');
   }
-});
 
-// Show Add player container when "Add More Players" is clicked:
+  _hideModal(e) {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+    // playerNameInput.focus();
 
-btnAddMorePlayers.addEventListener('click', function (e) {
-  containerAddPlayers.classList.remove('hidden');
-  btnLetsPlay.textContent = 'Keep Playing';
-  containerAddResetPlayers.classList.add('hidden');
-  nav.querySelector('p').classList.remove('hidden');
-});
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      modal.classList.add('hidden');
+      overlay.classList.add('hidden');
+      // playerNameInput.focus();
+    }
+  }
+
+  _showModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  }
+
+  _resetAllPlayers() {
+    localStorage.removeItem('players');
+    location.reload();
+  }
+}
+
+const app = new App();
+
+// note: what's left: style color picker,
